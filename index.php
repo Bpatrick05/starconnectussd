68fe367ef3e219ba7af0d7a64699c2dcbe3b6529
Patrick <byishimo.patrick@gmail.com>
2020-11-19 19:17:14 +0000

    change from xml to http request

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
+
 <?php
 
-ini_set('display_errors', 0);
-error_reporting(E_ALL & ~E_NOTICE);
+$input = $_POST['subscriberInput'];
+$sessionId = $_POST['sessionId'];
+$telephone = $_POST['msisdn'];
+$newRequest = $_POST['newRequest'];
+$actionFailed = false;
 
-require 'config.php';
-require 'library/settings.php';
 
-function __autoload($class) {
-    require LIBS . $class . ".php";
-}
+//=== Default data
+$lang = 'EN';
+$MAP_CODE = null;
+$display = "WELCOME TO TRACAR RWANDA";
+$session_ussd_user_data = null;
 
-$app = new Bootstrap();
+// Default config
+$arrayField['Freeflow'] = 'FB';
 
+
+    
+header('Content-type: UTF-8');
+header('Freeflow: '.$arrayField['Freeflow']);
+echo $display." ";
 
 ?>
+
